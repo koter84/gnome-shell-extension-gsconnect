cb9e1f87610baaea6a226b6792c59bc82184954c
Dennis Koot <koter84@gmail.com>
2020-03-31 11:52:29 +0200

    restore pulseaudio on 'nothing' volume option

--- src/service/plugins/telephony.js
+++ src/service/plugins/telephony.js
@@ -94,6 +94,10 @@ var Plugin = GObject.registerClass({
 
         if (pulseaudio) {
             switch (this.settings.get_string(`${eventType}-volume`)) {
+                case 'nothing':
+                    pulseaudio.restore();
+                    break;
+
                 case 'lower':
                     pulseaudio.lowerVolume();
                     break;
